98ae9279322e68efd3692cff106d98a81c902854
Krista Simmons <kristasimmons.design@gmail.com>
2023-01-28 09:33:58 -0800

    ViewNotes component structure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <NavBar />
+
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
 </template>
 
 <script setup>
 /*
   Imports
 */
-import NavBar from '@/components/Layout/NavBar.vue'
-
-
+import NavBar from "@/components/Layout/NavBar.vue";
 </script>
 
-
 <style>
-@import 'bulma/css/bulma.min.css';
+@import "bulma/css/bulma.min.css";
 </style>
