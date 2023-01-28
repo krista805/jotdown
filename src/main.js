c48a19d82c771f03a149687ddf25047ab5370b56
Krista Simmons <kristasimmons.design@gmail.com>
2023-01-28 08:33:14 -0800

    Adds Vue Router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
+import { createApp, useAttrs } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: []
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
