5ec27538b9c6845f6f38ddf6be7c84cd30c7a3b7
Krista Simmons <kristasimmons.design@gmail.com>
2023-01-29 10:57:52 -0800

    Add note component, pass note data as props

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -24,17 +24,7 @@
       </div>
     </div>
 
-    <div v-for="note in notes" :key="note.id" class="card mb-4">
-      <div class="card-content">
-        <div class="content">
-          {{ note.content }}
-        </div>
-      </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
-    </div>
+    <Note v-for="note in notes" :key="note.id" :note="note" />
   </div>
 </template>
 
@@ -43,6 +33,7 @@
     Imports
 */
 import { ref } from "vue"
+import Note from "@/components/Notes/Note.vue"
 
 /*
     Notes
