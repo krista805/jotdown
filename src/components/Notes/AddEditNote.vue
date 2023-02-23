6a442548bdbc0b9d05bdf61835c7f8694b1b1d0a
Krista Simmons <kristasimmons.design@gmail.com>
2023-02-23 15:01:33 -0800

    Creates component for add/edit textarea

---
+++ src/components/Notes/AddEditNote.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="notes">
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea
+            ref="newNoteRef"
+            class="textarea"
+            placeholder="Add a new note"
+          />
+        </div>
+      </div>
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <slot name="buttons" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="notes">
-    <div class="card has-background-success-dark p-4 mb-5">
+    <AddEditNote
+      ><template #buttons>
+        <button
+          @click="addNote"
+          class="button is-link has-background-success"
+          :disabled="!newNote"
+        >
+          Add New Note
+        </button></template
+      >
+    </AddEditNote>
+    <!-- <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
           <textarea
@@ -22,13 +33,9 @@
           </button>
         </div>
       </div>
-    </div>
+    </div> -->
 
-    <Note
-      v-for="note in storeNotes.notes"
-      :key="note.id"
-      :note="note"
-    />
+    <Note v-for="note in storeNotes.notes" :key="note.id" :note="note" />
   </div>
 </template>
 
@@ -38,6 +45,7 @@
 */
 import { ref } from "vue"
 import Note from "@/components/Notes/Note.vue"
+import AddEditNote from "@/components/Notes/AddEditNote.vue"
 import { useStoreNotes } from "@/stores/storeNotes"
 
 /*
@@ -52,10 +60,9 @@ const newNote = ref("")
 const newNoteRef = ref(null)
 
 const addNote = () => {
-    storeNotes.addNote(newNote.value)
+  storeNotes.addNote(newNote.value)
 
-    newNote.value = ""
-    newNoteRef.value.focus()
+  newNote.value = ""
+  newNoteRef.value.focus()
 }
-
 </script>
