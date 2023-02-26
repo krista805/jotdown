601f94ee2f161daab649bdae122c8875510228e3
Krista Simmons <kristasimmons.design@gmail.com>
2023-02-26 09:37:20 -0800

    Firebase npm package and config added

---
+++ src/js/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgPGqOVXDPcWO2I52Dq0GMxKAdfpbnhQo",
+  authDomain: "jotdown-ec85d.firebaseapp.com",
+  projectId: "jotdown-ec85d",
+  storageBucket: "jotdown-ec85d.appspot.com",
+  messagingSenderId: "78452130035",
+  appId: "1:78452130035:web:518f60e88a5d674a177786",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
